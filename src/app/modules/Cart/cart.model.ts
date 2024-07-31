afb1a0c7d27df5b0eb30f22e65ad35c83ff16d85
Naymul-NN <naymulislam108@gmail.com>
2024-07-31 11:32:27 +0600

    hanble apperror

--- src/app/modules/Cart/cart.model.ts
+++ src/app/modules/Cart/cart.model.ts
@@ -1,5 +1,7 @@
 import { model, Schema } from "mongoose";
 import { TproductCart } from "./cart.interface";
+import httpStatus from "http-status";
+import AppError from "../../errors/appError";
 
 const ProductCartSchema = new Schema<TproductCart>({
     
@@ -14,5 +16,16 @@ const ProductCartSchema = new Schema<TproductCart>({
     timestamps:true
 }
 );
+
+ProductCartSchema.pre('save', async function(next) {
+    
+        const existingCart = await ProductCart.findOne({ product: this.product });
+        if (existingCart) {
+                    throw new AppError(httpStatus.NOT_FOUND,'this product is already exist');
+                }
+                next();
+       
+    
+});
 
 export const ProductCart = model('cart', ProductCartSchema);
